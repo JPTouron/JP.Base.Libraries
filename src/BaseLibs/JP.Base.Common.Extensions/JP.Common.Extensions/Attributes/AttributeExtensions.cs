ab366f039ec833653bb4b2665b88bdbd288c95ce
JPTouron <JP.Touron@gmail.com>
2018-06-02 11:14:11 -0300

    Added attribute extensions class

---
+++ src/BaseLibs/JP.Base.Common.Extensions/JP.Common.Extensions/Attributes/AttributeExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace JP.Base.Common.Extensions.Attributes
+{
+    public static class AttributeExtensions
+    {
+        public static T GetAttribute<T>(this MemberInfo member, bool isRequired) where T : Attribute
+        {
+            var attribute = member.GetCustomAttributes(typeof(T), false).SingleOrDefault();
+
+            if (attribute == null && isRequired)
+            {
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "The {0} attribute must be defined on member {1}",
+                        typeof(T).Name,
+                        member.Name));
+            }
+
+            return (T)attribute;
+        }
+
+        public static MemberInfo GetPropertyInformation(Expression propertyExpression)
+        {
+            Debug.Assert(propertyExpression != null, "propertyExpression != null");
+            MemberExpression memberExpr = propertyExpression as MemberExpression;
+            if (memberExpr == null)
+            {
+                UnaryExpression unaryExpr = propertyExpression as UnaryExpression;
+                if (unaryExpr != null && unaryExpr.NodeType == ExpressionType.Convert)
+                    memberExpr = unaryExpr.Operand as MemberExpression;
+            }
+
+            if (memberExpr != null && memberExpr.Member.MemberType == MemberTypes.Property)
+                return memberExpr.Member;
+
+            return null;
+        }
+
+        public static string GetPropertyName<T>(Expression<Func<T, string>> propertyExpression)
+        {
+            var memberInfo = GetPropertyInformation(propertyExpression.Body);
+            if (memberInfo == null)
+            {
+                throw new ArgumentException(
+                    "No property reference expression was found.",
+                    "propertyExpression");
+            }
+            return memberInfo.Name;
+        }
+
+        public static string GetPropertyName<T, T2>(Expression<Func<T, T2>> propertyExpression)
+        {
+            var memberInfo = GetPropertyInformation(propertyExpression.Body);
+            if (memberInfo == null)
+            {
+                throw new ArgumentException(
+                    "No property reference expression was found.",
+                    "propertyExpression");
+            }
+            return memberInfo.Name;
+        }
+    }
+}
