e4e0565c7f44f8cf8fa6048a0c96f5b8381fb8e6
jptouron <jp.touron@gmail.com>
2018-06-11 15:36:12 -0300

    removed unnecesary sparing Execute methods produced on an earlier test

--- src/BaseLibs/JP.Base.Logic/BaseLogic.cs
+++ src/BaseLibs/JP.Base.Logic/BaseLogic.cs
@@ -1,6 +1,4 @@
-﻿using JP.Base.DAL.Model;
-using JP.Base.DAL.UnitOfWork;
-using System;
+﻿using JP.Base.DAL.UnitOfWork;
 
 namespace JP.Base.Logic
 {
@@ -12,26 +10,6 @@ namespace JP.Base.Logic
         public BaseLogic(IUoWFactory<TIUnitOfWork> factory)
         {
             this.factory = factory;
-        }
-
-        public Tresult Execute<Tresult>(Func<IUoWFactory<TIUnitOfWork>, Tresult> funcy)
-        {
-            return funcy.Invoke(factory);
-        }
-
-        public Tresult Execute<Tresult, TParam>(Func<IUoWFactory<TIUnitOfWork>, Tresult> funcy, TParam param)
-        {
-            return funcy.Invoke(factory);
-        }
-
-        public void Execute(Action<IUoWFactory<TIUnitOfWork>> action)
-        {
-            action.Invoke(factory);
-        }
-
-        public void Execute(Action<IUoWFactory<TIUnitOfWork>, BaseModel<int>> action, BaseModel<int> param)
-        {
-            action.Invoke(factory, param);
         }
     }
 }
